33a1624630f13f133fe9dd74bc2415ac04f5bffc
Park Myeong Soo <lyine0924@gmail.com>
2021-05-18 09:02:24 +0900

    add Diary Entities

---
+++ Tooda/Sources/Entities/Diary/DiaryImage.swift
@@ -0,0 +1,25 @@
+//
+//  DiaryImage.swift
+//  Tooda
+//
+//  Created by lyine on 2021/05/18.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import Foundation
+
+struct DiaryImage: Codable {
+	var id: Int
+	var imageURL: String
+	
+	private enum CodingKeys: String, CodingKey {
+		case id = "id"
+		case imageURL = "image"
+	}
+	
+	init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		id = try values.decode(Int.self, forKey: .id)
+		imageURL = try values.decode(String.self, forKey: .imageURL)
+	}
+}
---
+++ Tooda/Sources/Entities/Diary/DiaryStock.swift
@@ -0,0 +1,37 @@
+//
+//  DiaryStock.swift
+//  Tooda
+//
+//  Created by lyine on 2021/05/18.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import Foundation
+
+enum StockChangeState: String, Codable {
+	case rise
+	case even
+	case fall
+}
+
+struct DiaryStock: Codable {
+	var id: Int
+	var name: String
+	var change: StockChangeState?
+	var changeRate: Float?
+	
+	private enum CodingKeys: String, CodingKey {
+		case id = "id"
+		case name = "name"
+		case change = "change"
+		case changeRate = "change_rate"
+	}
+	
+	init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		id = try values.decode(Int.self, forKey: .id)
+		name = try values.decode(String.self, forKey: .name)
+		change = try values.decode(StockChangeState.self, forKey: .change)
+		changeRate = try values.decode(Float.self, forKey: .changeRate)
+	}
+}
