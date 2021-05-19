0376b0e78b21d36efdc314ff91d2dd963ec6f890
myeongsoo <myeongsoo.park@bbros.kr>
2021-05-19 18:07:57 +0900

    등록/상세 시 사용할 셀 틀 추가

---
+++ Tooda/Sources/Scenes/CreateDiary/Cells/DiaryImageCellReactor.swift
@@ -0,0 +1,38 @@
+//
+//  DiaryImageCellReactor.swift
+//  Tooda
+//
+//  Created by Lyine on 2021/05/19.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import ReactorKit
+
+final class DiaryImageCellReactor: Reactor {
+	enum Action {
+		
+	}
+	
+	enum Mutation {
+		
+	}
+	
+	struct State {
+		
+	}
+	
+	let initialState: State
+	
+	init() {
+		initialState = State()
+	}
+	
+	func mutate(action: Action) -> Observable<Mutation> {
+		return .empty()
+	}
+	
+	func reduce(state: State, mutation: Action) -> State {
+		var newState = state
+		return newState
+	}
+}
