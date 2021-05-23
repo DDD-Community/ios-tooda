2857f19b272cca592584c50ae35eb0d9d3b313d4
jinsu <jinsu3758@naver.com>
2021-05-23 16:30:37 +0900

    AppInject indent 2 space로 변경

--- Tooda/Sources/Core/AppInject.swift
+++ Tooda/Sources/Core/AppInject.swift
@@ -10,42 +10,42 @@ import Swinject
 import Foundation
 
 protocol AppInjectRegister {
-    func registerCore()
+  func registerCore()
 }
 
-protocol AppInjectResolve{
-    func resolve<Object>(_ serviceType: Object.Type) -> Object
-    func makeViewController(from scene: Scene) -> UIViewController
+protocol AppInjectResolve {
+  func resolve<Object>(_ serviceType: Object.Type) -> Object
+  func makeViewController(from scene: Scene) -> UIViewController
 }
 
 final class AppInject: AppInjectRegister, AppInjectResolve {
-    
-    private let container: Container
-    
-    init(container: Container) {
-        self.container = container
+  
+  private let container: Container
+  
+  init(container: Container) {
+    self.container = container
+  }
+  
+  func registerCore() {
+    container.register(NetworkingProtocol.self) { _ in
+      Networking(logger: [AccessTokenPlugin()])
     }
-    
-    func registerCore() {
-        container.register(NetworkingProtocol.self) { _ in
-            Networking(logger: [AccessTokenPlugin()])
-        }
+  }
+  
+  func resolve<Object>(_ serviceType: Object.Type) -> Object {
+    return container.resolve(serviceType)!
+  }
+  
+  // TODO: 분리할 예정
+  func makeViewController(from scene: Scene) -> UIViewController {
+    switch scene {
+    case .home:
+      let reactor = HomeReactor(
+        dependency: .init(
+          service: resolve(NetworkingProtocol.self)
+        )
+      )
+      return HomeViewController(reactor: reactor)
     }
-    
-    func resolve<Object>(_ serviceType: Object.Type) -> Object {
-        return container.resolve(serviceType)!
-    }
-    
-    // TODO: 분리할 예정
-    func makeViewController(from scene: Scene) -> UIViewController {
-        switch scene {
-        case .home:
-            let reactor = HomeReactor(
-                dependency: .init(
-                    service: resolve(NetworkingProtocol.self)
-                )
-            )
-            return HomeViewController(reactor: reactor)
-        }
-    }
+  }
 }
