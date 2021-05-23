ff8bef2dd2bf12a00e479f6ddd7c7c89e699642d
jinsu <jinsu3758@naver.com>
2021-05-23 20:17:52 +0900

    AppAppearance에 NavigationStyle 추가

--- Tooda/Sources/Core/AppAppearance.swift
+++ Tooda/Sources/Core/AppAppearance.swift
@@ -10,16 +10,63 @@ import UIKit
 
 final class AppApppearance {
   
-  static func configureAppeance() {
-    let navigationbar = UINavigationBar.appearance()
-    let backImage = UIImage(type: .backBarButton)
+  enum NavigationBarStyle {
+    case normal
+    case clear
     
-    navigationbar.tintColor = .black
-    navigationbar.setBackgroundImage(UIImage(), for: .default)
-    navigationbar.shadowImage = UIImage()
-    navigationbar.backgroundColor = .clear
+    var tintColor: UIColor {
+      return .black
+    }
+    
+    var isTranslucent: Bool {
+      switch self {
+      case .clear:
+        return false
+      case .normal:
+        return true
+      }
+    }
+    
+    var backButtonImage: UIImage? {
+      return UIImage(type: .backBarButton)
+    }
+    
+    var shadowImage: UIImage? {
+      switch self {
+      case .clear:
+        return UIImage()
+      case .normal:
+        return nil
+      }
+    }
+    
+    var backgroundImage: UIImage? {
+      switch self {
+      case .clear:
+        return UIImage()
+      case .normal:
+        return nil
+      }
+    }
+  }
+  
+  static func configureAppearance() {
+    let navigationbar = UINavigationBar.appearance()
+    let style: NavigationBarStyle = .clear
+    
+    navigationbar.tintColor = style.tintColor
+    navigationbar.setBackgroundImage(style.backgroundImage, for: .default)
+    navigationbar.shadowImage = style.shadowImage
     navigationbar.backItem?.title = ""
-    navigationbar.backIndicatorImage = backImage
-    navigationbar.backIndicatorTransitionMaskImage = backImage
+    navigationbar.backIndicatorImage = style.backButtonImage
+    navigationbar.backIndicatorTransitionMaskImage = style.backButtonImage
+  }
+  
+  static func updateNavigaionBarAppearance(_ navigationbar: UINavigationBar?, with style: NavigationBarStyle) {
+    navigationbar?.tintColor = style.tintColor
+    navigationbar?.setBackgroundImage(style.backgroundImage, for: .default)
+    navigationbar?.shadowImage = style.shadowImage
+    navigationbar?.backIndicatorImage = style.backButtonImage
+    navigationbar?.backIndicatorTransitionMaskImage = style.backButtonImage
   }
 }
