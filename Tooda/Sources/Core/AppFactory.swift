be5a9ea537b6b03e42a774683803ed26ec24c12c
jinsu <jinsu3758@naver.com>
2021-05-23 17:02:45 +0900

    AppFactory 추가

---
+++ Tooda/Sources/Core/AppFactory.swift
@@ -0,0 +1,41 @@
+//
+//  AppFactory.swift
+//  Tooda
+//
+//  Created by jinsu on 2021/05/23.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import Foundation
+
+import Swinject
+
+protocol AppFactoryType {
+  func makeViewController(from scene: Scene) -> UIViewController
+}
+
+final class AppFactory: AppFactoryType {
+
+  struct Dependency {
+    let appInject: AppInjectRegister & AppInjectResolve
+  }
+  
+  private let dependency: Dependency
+  
+  init(dependency: Dependency) {
+    self.dependency = dependency
+  }
+  
+  func makeViewController(from scene: Scene) -> UIViewController {
+    switch scene {
+    case .home:
+      let reactor = HomeReactor(
+        dependency: .init(
+          service: self.dependency.appInject.resolve(NetworkingProtocol.self),
+          coordinator: self.dependency.appInject.resolve(AppCoordinatorType.self)
+        )
+      )
+      return HomeViewController(reactor: reactor)
+    }
+  }
+}
