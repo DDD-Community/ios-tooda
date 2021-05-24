47f42c7d7bee361dc7946e8f2c4cc8082269dcf3
Park Myeong Soo <lyine0924@gmail.com>
2021-05-24 13:41:16 +0900

    NoteImage 관련 Reactor 구현

--- Tooda/Sources/Scenes/CreateNote/Cells/Reactors/NoteImageCellReactor.swift
+++ Tooda/Sources/Scenes/CreateNote/Cells/Reactors/NoteImageCellReactor.swift
@@ -9,30 +9,53 @@
 import ReactorKit
 
 final class NoteImageCellReactor: Reactor {
+  
+  let scheduler: Scheduler = MainScheduler.asyncInstance
+  
+  struct Dependency {
+    let factory: NoteImageSectionType
+  }
+  
   enum Action {
-
+    case initiailizeSection
+    // TODO: 이미지 추가 로직
+//    case addImage
   }
 
   enum Mutation {
-
+    case fetchSection([NoteImageSection])
+//    case addImage([NoteImageSectionItem])
   }
 
   struct State {
-
+    var sections: [NoteImageSection]
   }
 
   let initialState: State
+  let dependency: Dependency
 
-  init() {
-    initialState = State()
+  init(dependency: Dependency) {
+    self.dependency = dependency
+    initialState = State(sections: [])
   }
 
   func mutate(action: Action) -> Observable<Mutation> {
-    return .empty()
+    switch action {
+      case .initiailizeSection:
+        return .just(Mutation.fetchSection(self.generateSection(images: [])))
+    }
   }
-
-  func reduce(state: State, mutation: Action) -> State {
+  
+  func reduce(state: State, mutation: Mutation) -> State {
     var newState = state
-    return newState
+    switch mutation {
+      case .fetchSection(let sections):
+        newState.sections = sections
+        return newState
+    }
+  }
+  
+  private func generateSection(images: [NoteImage]) -> [NoteImageSection] {
+    return self.dependency.factory(images)
   }
 }
