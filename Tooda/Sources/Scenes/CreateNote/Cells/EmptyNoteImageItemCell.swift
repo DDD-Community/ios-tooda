ea68bdfa6779507ed1d9fc2ca100a7eb4d2e4ec2
Park Myeong Soo <lyine0924@gmail.com>
2021-05-24 13:40:29 +0900

    emptyNoteImageItemCell 추가

---
+++ Tooda/Sources/Scenes/CreateNote/Cells/EmptyNoteImageItemCell.swift
@@ -0,0 +1,50 @@
+//
+//  EmptyNoteImageItemCell.swift
+//  Tooda
+//
+//  Created by lyine on 2021/05/24.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import UIKit
+
+import ReactorKit
+
+import SnapKit
+
+class EmptyNoteImageItemCell: BaseCollectionViewCell, View {
+  typealias Reactor = EmptyNoteImageItemCellReactor
+  
+  var disposeBag: DisposeBag = DisposeBag()
+  
+  let containerView = UIView().then {
+    $0.backgroundColor = UIColor(type: .gray3)
+    $0.layer.cornerRadius = 8.0
+    $0.layer.masksToBounds = true
+  }
+  
+  func configure(reactor: Reactor) {
+    super.configure()
+    self.reactor = reactor
+  }
+  
+  func bind(reactor: Reactor) {
+    
+  }
+  
+  override func configureUI() {
+    super.configureUI()
+    
+    [containerView].forEach {
+      self.contentView.addSubview($0)
+    }
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    
+    containerView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+}
