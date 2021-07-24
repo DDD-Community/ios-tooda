66b17f9d71342c603e947bec30bb9c45443d8617
jinsu3758 <jinsu3758@naver.com>
2021-07-24 19:40:11 +0900

    NSAttributedString Extension 추가

--- Tooda/Sources/Common/Extensions/NSAttributedString+Extension.swift
+++ Tooda/Sources/Common/Extensions/NSAttributedString+Extension.swift
@@ -17,4 +17,53 @@ extension NSAttributedString {
     result.append(right)
     return result
   }
+
+  func replace(key: String, value: String, style: Style) -> NSAttributedString {
+    guard let mutableAttr = self.mutableCopy() as? NSMutableAttributedString else { return self }
+    let target = "{" + key + "}"
+    guard let range = self.string.range(of: target) else { return self }
+
+    mutableAttr.replaceCharacters(
+      in: NSRange(range, in: self.string),
+      with: value.styled(with: style)
+    )
+
+    return mutableAttr
+  }
+
+  func replace(key: String, value: String) -> NSAttributedString {
+    guard let mutableAttr = self.mutableCopy() as? NSMutableAttributedString else { return self }
+    let target = "{" + key + "}"
+    guard let range = self.string.range(of: target) else { return self }
+
+    mutableAttr.replaceCharacters(
+      in: NSRange(range, in: self.string),
+      with: value
+    )
+
+    return mutableAttr
+  }
+
+  func replace(value: String, style: Style) -> NSAttributedString {
+    guard let mutableAttr = self.mutableCopy() as? NSMutableAttributedString else { return self }
+    guard let range = self.string.range(of: value) else { return self }
+
+    mutableAttr.replaceCharacters(
+      in: NSRange(range, in: self.string),
+      with: value.styled(with: style)
+    )
+
+    return mutableAttr
+  }
+
+  class func composed(of strings: [NSAttributedString], separator: NSAttributedString) -> NSAttributedString {
+    return strings.reduce(strings[0], { (s1, s2) -> NSAttributedString in
+      let result = NSMutableAttributedString()
+      result.append(s1)
+      result.append(separator)
+      result.append(s2)
+
+      return result
+    })
+  }
 }
