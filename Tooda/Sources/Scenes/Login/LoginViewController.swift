2e302afe8d6c31978f75b787eadb900714c12d23
Jaewook <hjw0911@gmail.com>
2021-07-26 00:47:06 +0900

    feat: LoginViewController 레이아웃 수정

--- Tooda/Sources/Scenes/Login/LoginViewController.swift
+++ Tooda/Sources/Scenes/Login/LoginViewController.swift
@@ -6,9 +6,29 @@
 //  Copyright © 2021 DTS. All rights reserved.
 //
 
-import Foundation
+import SnapKit
+import Then
 
 class LoginViewController: BaseViewController<LoginReactor> {
+  
+  // MARK: Constants
+
+  private enum Typo {
+    static let title = TextStyle.title(color: .white)
+  }
+  
+  // MARK: - UI Components
+  
+  private let mainImageView = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+  }
+  
+  private let loginButton = UIButton().then {
+    $0.setAttributedTitle(
+      "시작하기".styled(with: Typo.title),
+      for: .normal
+    )
+  }
   
   // MARK: - Con(De)structor
   
@@ -25,5 +45,27 @@ class LoginViewController: BaseViewController<LoginReactor> {
   
   deinit {
     print("\(#file) deinitialized")
+  }
+  
+  // MARK: - SetupUI
+  
+  override func configureConstraints() {
+    view.addSubviews(
+      mainImageView,
+      loginButton
+    )
+    
+    mainImageView.snp.makeConstraints {
+      $0.leading.equalToSuperview().offset(79)
+      $0.top.equalToSuperview().offset(114)
+      $0.width.equalTo(243)
+      $0.height.equalTo(515)
+    }
+    
+    loginButton.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(20)
+      $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-34)
+      $0.height.equalTo(56)
+    }
   }
 }
