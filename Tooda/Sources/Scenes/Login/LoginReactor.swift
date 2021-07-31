3ebd7b7077779eac5c071a38b25324613e143c80
Jaewook <hjw0911@gmail.com>
2021-07-31 16:49:41 +0900

    feat: reduce, mutate func 추가 in LoginReactor

--- Tooda/Sources/Scenes/Login/LoginReactor.swift
+++ Tooda/Sources/Scenes/Login/LoginReactor.swift
@@ -18,19 +18,59 @@ final class LoginReactor: Reactor {
   }
   
   enum Action {
-    
+    case login
+  }
+  
+  enum Mutation {
+    case setAppToken(token: AppToken)
+    case setIsAuthorized(isAuthorized: Bool)
   }
   
   struct State {
-    
+    var isAuthorized: Bool
   }
   
   private let dependency: Dependency
   
-  let initialState: State = State()
+  private let disposeBag = DisposeBag()
   
+  let initialState: State = State(isAuthorized: false)
   
   init(dependency: Dependency) {
     self.dependency = dependency
+  }
+}
+
+// MARK: - mutate & reduce
+extension LoginReactor {
+  func mutate(action: Action) -> Observable<Mutation> {
+    switch action {
+    case .login:
+      return loginMutation()
+    }
+  }
+  
+  private func loginMutation() -> Observable<Mutation> {
+    return dependency.service
+      .request(AuthAPI.signUp(uuidString: deviceUUID() ?? ""))
+      .map(AppToken.self)
+      .asObservable()
+      .flatMap { token -> Observable<Mutation> in
+        let mutation = Mutation.setAppToken(token: token)
+        return Observable<Mutation>.just(mutation)
+    }
+  }
+
+  func reduce(state: State, mutation: Mutation) -> State {
+    var newState = state
+    
+    switch mutation {
+    case let .setAppToken(token):
+      print(token)
+    case let .setIsAuthorized(isAuthorized):
+      newState.isAuthorized = isAuthorized
+    }
+
+    return newState
   }
 }
