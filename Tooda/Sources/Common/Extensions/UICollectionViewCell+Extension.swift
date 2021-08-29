62ad22f39b708b95cab583f5e42bc45c8552a35d
myeongsoo <lyine0924@gmail.com>
2021-08-29 15:20:41 +0900

    CellType, ViewType Protocol 추가, reueseIdentifeir 변경
    - register, dequeue extension 메소드 추가

--- Tooda/Sources/Common/Extensions/UICollectionViewCell+Extension.swift
+++ Tooda/Sources/Common/Extensions/UICollectionViewCell+Extension.swift
@@ -8,9 +8,60 @@
 
 import UIKit
 
+public enum SupplementaryViewKind: String {
+  case header, footer
+  
+  public init?(rawValue: String) {
+    switch rawValue {
+      case UICollectionView.elementKindSectionHeader: self = .header
+      case UICollectionView.elementKindSectionFooter: self = .footer
+      default: return nil
+    }
+  }
+  
+  public var rawValue: String {
+    switch self {
+      case .header: return UICollectionView.elementKindSectionHeader
+      case .footer: return UICollectionView.elementKindSectionFooter
+    }
+  }
+}
+
+protocol CellType: AnyObject {
+  static var reuseIdentifier: String { get }
+}
+
+protocol ViewType: AnyObject {}
+
 // MARK: UICollectionViewCell
-extension UICollectionViewCell {
-  static var reuseIdentifierName: String {
+extension UICollectionViewCell: CellType {
+  static var reuseIdentifier: String {
     return String(describing: self)
+  }
+}
+
+extension UICollectionReusableView: ViewType {}
+
+// MARK: Cell
+
+extension UICollectionView {
+  func dequeue<T>(_ cellType: T.Type, indexPath: IndexPath) -> T where T: CellType {
+    return self.dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as! T
+  }
+  
+  func register<T>(_ cellType: T.Type) where T: CellType {
+    self.register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
+  }
+}
+
+// MARK: Header & FooterView
+
+extension UICollectionView {
+  func dequeue<T>(_ cellType: T.Type, kind: SupplementaryViewKind, indexPath: IndexPath) -> T where T: CellType, T: ViewType {
+    return self.dequeueReusableSupplementaryView(ofKind: kind.rawValue, withReuseIdentifier: T.reuseIdentifier, for: indexPath) as! T
+  }
+  
+  func register<T>(_ cellType: T.Type, kind: SupplementaryViewKind) where T: CellType, T: ViewType {
+    self.register(T.self, forSupplementaryViewOfKind: kind.rawValue, withReuseIdentifier: T.reuseIdentifier)
   }
 }
