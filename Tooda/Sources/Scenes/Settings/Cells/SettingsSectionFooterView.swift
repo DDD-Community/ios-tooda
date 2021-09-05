2def6315be15b5332cd3144bf6f3e3c4924429c6
Jaewook <hjw0911@gmail.com>
2021-09-06 01:37:50 +0900

    feat: SettingsSectionFooterView 파일 추가

---
+++ Tooda/Sources/Scenes/Settings/Cells/SettingsSectionFooterView.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsSectionFooterView.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/09/06.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import UIKit
+
+class SettingsSectionFooterView: UITableViewCell {
+  
+}
