8cd1d3308f417e4487a68b7fcc6659a426a500dd
Jaewook <hjw0911@gmail.com>
2021-09-05 22:01:19 +0900

    feat: SettingsViewController 파일 추가

---
+++ Tooda/Sources/Scenes/Settings/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/09/05.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
