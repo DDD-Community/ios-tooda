840a1ac340c0e7d0e63fd1936c0e7b203dccc2b6
Jaewook <hjw0911@gmail.com>
2021-09-18 20:49:45 +0900

    feat: array safe 접근 extension 추가

---
+++ Tooda/Sources/Common/Extensions/Array+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Array+Extension.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/09/18.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    subscript (safe index: Int) -> Element? {
+        return indices ~= index ? self[index] : nil
+    }
+}
