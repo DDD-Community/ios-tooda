bff46b155bfd50009c4a38d47e803a7745037767
myeongsoo <lyine0924@gmail.com>
2021-09-18 19:45:48 +0900

    addImage endpoint 추가

--- Tooda/Sources/Networking/API/NoteAPI.swift
+++ Tooda/Sources/Networking/API/NoteAPI.swift
@@ -14,63 +14,71 @@ enum NoteAPI {
   case create(diary: Note)
   case list(limit: Int, cursor: Int)
   case delete(id: String)
+  case addImage(data: Data)
   // TODO: update는 서버 스펙 전달 받는대로
 }
 
 extension NoteAPI: BaseAPI {
   var path: String {
     switch self {
-    case .create:
-      return "/diary"
-    case .list(let limit, let cursor):
-      return "/diary?limit=\(limit)&cursor\(cursor)"
-    case .delete(let id):
-      return "/diary/\(id)"
+      case .create:
+        return "/diary"
+      case .list(let limit, let cursor):
+        return "/diary?limit=\(limit)&cursor\(cursor)"
+      case .addImage:
+        return "diary/image"
+      case .delete(let id):
+        return "/diary/\(id)"
     }
   }
-
+  
   var method: Moya.Method {
     switch self {
-    case .create:
-      return .post
-    case .list:
-      return .get
-    case .delete:
-      return .delete
+      case .create, .addImage:
+        return .post
+      case .list:
+        return .get
+      case .delete:
+        return .delete
     }
   }
-
+  
   var task: Task {
     guard let parameters = parameters else { return .requestPlain }
     var body: [String: Any] = [:]
-
+    
     switch self {
-    case .create(let note):
-      // TODO: diary 파라미터 작성
-      return .requestCompositeParameters(bodyParameters: body, bodyEncoding: JSONEncoding.default, urlParameters: parameters)
-    case .list, .delete:
-      return .requestParameters(parameters: parameters, encoding: parameterEncoding)
+      case .create(let note):
+        // TODO: diary 파라미터 작성
+        return .requestCompositeParameters(bodyParameters: body, bodyEncoding: JSONEncoding.default, urlParameters: parameters)
+      case .addImage(let imageData):
+        let imageData = MultipartFormData(provider: .data(imageData), name: "file", fileName: "image.jpeg", mimeType: "image/jpeg")
+        let multipartData = [imageData]
+        
+        return .uploadMultipart(multipartData)
+      case .list, .delete:
+        return .requestParameters(parameters: parameters, encoding: parameterEncoding)
     }
   }
-
+  
   var parameters: [String: Any]? {
-
+    
     let defaultParameters: [String: Any] = [:]
-
+    
     let parameters: [String: Any] = defaultParameters
-
+    
     switch self {
-    case .create, .list, .delete:
-      return parameters
+      case .create, .list, .delete, .addImage:
+        return parameters
     }
   }
-
+  
   var parameterEncoding: ParameterEncoding {
     switch self {
-    case .create, .delete:
-      return JSONEncoding.default
-    case .list:
-      return URLEncoding.queryString
+      case .create, .delete, .addImage:
+        return JSONEncoding.default
+      case .list:
+        return URLEncoding.queryString
     }
   }
 }
