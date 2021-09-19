42a3d37fbc415eca8a22ba35e5f72e41ef1938f6
Jaewook <hjw0911@gmail.com>
2021-09-20 02:28:34 +0900

    chore: SettingsSectionModel 파일 추가

---
+++ Tooda/Sources/Entities/Settings/SettingsSectionModel.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsSectionModel.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/09/19.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import Foundation
+
+class SettingsSectionModel {
+  
+}
