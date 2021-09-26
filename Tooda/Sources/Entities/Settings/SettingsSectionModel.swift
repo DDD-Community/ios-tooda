af6c112b9de3525bea66f9ed56e65dcfe6532f71
Jaewook <hjw0911@gmail.com>
2021-09-26 18:32:31 +0900

    feat: SettingSectionModel 수정

--- Tooda/Sources/Entities/Settings/SettingsSectionModel.swift
+++ Tooda/Sources/Entities/Settings/SettingsSectionModel.swift
@@ -6,8 +6,48 @@
 //  Copyright © 2021 DTS. All rights reserved.
 //
 
-import Foundation
+import RxDataSources
 
-class SettingsSectionModel {
-  
+struct SettingsSectionModel {
+  enum SectionType: Int {
+    case notification
+    case etc
+    
+    var title: String {
+      switch self {
+      case .notification:
+        return "알림 설정"
+      case .etc:
+        return "기타"
+      }
+    }
+    
+    var footerTitle: String? {
+      switch self {
+      case .notification:
+        return nil
+      case .etc:
+        return "현재 버전 1.0.0"
+      }
+    }
+  }
+  let identity: SectionType
+  var items: [SettingsItem]
+}
+
+extension SettingsSectionModel: SectionModelType {
+  init(
+    original: SettingsSectionModel,
+    items: [SettingsItem]
+  ) {
+    self = .init(
+      identity: original.identity,
+      items: items
+    )
+  }
+}
+
+enum SettingsItem {
+  case interactive(InteractiveSettingsInfo)
+  case plain(PlainSettingsInfo)
 }
