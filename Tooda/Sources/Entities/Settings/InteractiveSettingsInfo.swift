6e18d448dca91a7c3ddf87af1d3b16db3beab61e
Jaewook <hjw0911@gmail.com>
2021-09-26 17:56:53 +0900

    feat: InteractiveSettingsInfo 모델 추가

---
+++ Tooda/Sources/Entities/Settings/InteractiveSettingsInfo.swift
@@ -0,0 +1,15 @@
+//
+//  InteractiveSettingsInfo.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/09/20.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import Foundation
+
+struct InteractiveSettingsInfo {
+  let title: String
+  let description: String
+  let isOn: Bool
+}
