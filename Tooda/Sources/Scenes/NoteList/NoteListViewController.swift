6f4139bfed6406bc0a41257b6532533bdff8e1b6
Jaewook <hjw0911@gmail.com>
2021-09-28 02:05:00 +0900

    feat: NoteListViewController 파일 추가

---
+++ Tooda/Sources/Scenes/NoteList/NoteListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NoteListViewController.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/09/28.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import UIKit
+
+class NoteListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
