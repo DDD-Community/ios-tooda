0cf177719795597a4cb708015ecb713d7a1d19f0
Jaewook <hjw0911@gmail.com>
2021-10-24 16:52:02 +0900

    feat: NoteListCell 추가 및 base layout 구현

---
+++ Tooda/Sources/Scenes/NoteList/Cells/NoteListCell.swift
@@ -0,0 +1,74 @@
+//
+//  NoteListCell.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/10/24.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import UIKit
+
+import SnapKit
+
+class NoteListCell: BaseTableViewCell {
+  
+  private let cardView = UIView()
+  
+  private let emojiImageView = UIImageView()
+  
+  private let titleLabel = UILabel()
+  
+  private let recordDateLabel = UILabel()
+  
+  private let descriptionLabel = UILabel()
+  
+  private let mainImageView = UIImageView()
+  
+
+  override func configureUI() {
+    super.configureUI()
+    contentView.addSubview(cardView)
+    cardView.addSubviews(
+      emojiImageView,
+      titleLabel,
+      recordDateLabel,
+      descriptionLabel,
+      mainImageView
+    )
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    cardView.snp.makeConstraints {
+      $0.top.equalToSuperview().inset(6)
+      $0.leading.trailing.equalToSuperview().inset(20)
+      $0.bottom.equalToSuperview().inset(6)
+    }
+    
+    emojiImageView.snp.makeConstraints {
+      $0.leading.equalToSuperview().inset(22)
+      $0.top.equalToSuperview().inset(27)
+      $0.width.height.equalTo(20)
+    }
+    
+    titleLabel.snp.makeConstraints {
+      $0.centerY.equalTo(emojiImageView)
+      $0.leading.equalTo(emojiImageView.snp.trailing).offset(9)
+    }
+    
+    recordDateLabel.snp.makeConstraints {
+      $0.leading.equalTo(emojiImageView)
+      $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+    }
+    
+    descriptionLabel.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(20)
+      $0.top.equalTo(recordDateLabel.snp.bottom).offset(16)
+    }
+    
+    mainImageView.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(20)
+      $0.top.equalTo(descriptionLabel.snp.bottom).offset(16)
+    }
+  }
+}
