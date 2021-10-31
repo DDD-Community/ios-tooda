02717ca56def6fbd3728385bad951ccc2f6ef26a
Myeong Soo <lyine0924@gmail.com>
2021-10-31 11:48:22 +0900

    종목추가 ViewController Boilerplate 코드 추가

---
+++ Tooda/Sources/Scenes/AddStock/AddStockReactor.swift
@@ -0,0 +1,48 @@
+  //
+  //  AddStockReactor.swift
+  //  Tooda
+  //
+  //  Created by Lyine on 2021/10/31.
+  //  Copyright © 2021 DTS. All rights reserved.
+  //
+
+import Foundation
+
+import ReactorKit
+import RxSwift
+
+final class AddStockReactor: Reactor {
+  
+  struct Depdendency {
+    
+  }
+  
+  // MARK: Reactor
+  
+  enum Action {
+    
+  }
+  
+  enum Mutation {
+    
+  }
+  
+  struct Dependency {
+    
+  }
+  
+  struct State {
+    
+  }
+  
+  init(dependency: Dependency) {
+    self.dependency = dependency
+    self.initialState = State()
+  }
+  
+    // MARK: Properties
+  
+  let dependency: Dependency
+  
+  let initialState: State
+}
