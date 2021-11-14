91823c9cf1f5b71f93e31149a087b2cf027931ae
Jaewook <hjw0911@gmail.com>
2021-11-14 18:03:41 +0900

    feat: data decoding 수정

--- Tooda/Sources/Entities/Note/NoteLink.swift
+++ Tooda/Sources/Entities/Note/NoteLink.swift
@@ -16,7 +16,6 @@ struct NoteLink: Codable {
 	var url: String?
 	var imageURL: String?
 	var createdAt: String?
-	var updatedAt: String?
 	
 	private enum CodingKeys: String, CodingKey {
 		case id = "id"
@@ -26,18 +25,5 @@ struct NoteLink: Codable {
 		case url = "ogUrl"
 		case imageURL = "ogImage"
 		case createdAt = "createdAt"
-		case updatedAt = "updatedAt"
-	}
-	
-	init(from decoder: Decoder) throws {
-		let values = try decoder.container(keyedBy: CodingKeys.self)
-		id = try values.decode(Int.self, forKey: .id)
-		title = try values.decode(String.self, forKey: .title)
-		description = try values.decode(String?.self, forKey: .description)
-		siteName = try values.decode(String?.self, forKey: .siteName)
-		url = try values.decode(String?.self, forKey: .url)
-		imageURL = try values.decode(String?.self, forKey: .imageURL)
-		createdAt = try values.decode(String?.self, forKey: .createdAt)
-		updatedAt = try values.decode(String?.self, forKey: .updatedAt)
 	}
 }
