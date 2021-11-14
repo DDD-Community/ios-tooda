14a0306c7fb4679b52518576aaf25bfa921b4c69
myeongsoo <lyine0924@gmail.com>
2021-11-14 17:17:14 +0900

    변동률 입력 화면에서 UI 요소를 추가했어요.

--- Tooda/Sources/Scenes/AddStock/StockRateInputViewController.swift
+++ Tooda/Sources/Scenes/AddStock/StockRateInputViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 import ReactorKit
 import SnapKit
 import Then
-  // ReactorViewController
 
 class ReactorViewController: BaseViewController<StockRateInputReactor> {
   typealias Reactor = StockRateInputReactor
+  
+  // MARK: Enum
+  
+  private enum Font {
+    static let title = TextStyle.titleBold(color: .gray1)
+    static let descprtion = TextStyle.body(color: .gray3)
+    static let searchField = TextStyle.body(color: .gray1)
+  }
+  
+  private enum Color {
+    static let borderLineGray: UIColor? = UIColor(type: .gray3)
+  }
   
   init(reactor: Reactor) {
     defer {
       self.reactor = reactor
     }
     super.init()
+  }
+  
+  // MARK: Properties
+  
+  // MARK: UI Properties
+  
+  private let closeBarButton = UIBarButtonItem(
+    image: UIImage(type: .closeButton)?.withRenderingMode(.alwaysOriginal),
+    style: .plain,
+    target: nil,
+    action: nil
+  )
+  
+  private let titleLabel = UILabel().then {
+    $0.numberOfLines = 0
+    $0.sizeToFit()
+  }
+  
+  private let descriptionLabel = UILabel().then {
+    $0.attributedText = "이 종목이 얼마나 변동했나요?".styled(with: Font.descprtion)
+    $0.numberOfLines = 1
+    $0.sizeToFit()
+  }
+  
+  private let textFieldBackgroundView = UIView().then {
+    $0.clipsToBounds = true
+    $0.layer.cornerRadius = 8.0
+    $0.layer.borderColor = Color.borderLineGray?.cgColor
+    $0.layer.borderWidth = 1.0
+  }
+  
+  private lazy var textField = UITextField().then {
+    $0.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+  }
+  
+  private let percentLabel = UILabel().then {
+    $0.attributedText = "%".styled(with: TextStyle.subTitleBold(color: .gray1))
+    $0.textAlignment = .right
+    $0.numberOfLines = 1
+    $0.sizeToFit()
   }
   
   required init?(coder: NSCoder) {
@@ -41,5 +92,19 @@ class ReactorViewController: BaseViewController<StockRateInputReactor> {
   
   override func bind(reactor: Reactor) {
     super.bind(reactor: reactor)
+  }
+  
+  @objc
+  func textFieldDidChange(textField: UITextField) {
+    textField.attributedText = textField.text?.styled(with: Font.searchField)
+  }
+}
+
+// MARK: - Extensions
+
+extension ReactorViewController {
+  private func initializeNavigation() {
+    self.navigationItem.title = "종목 기록하기"
+    self.navigationItem.rightBarButtonItem = closeBarButton
   }
 }
