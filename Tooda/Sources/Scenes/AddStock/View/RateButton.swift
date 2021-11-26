6bcc166b81d01f27f41cd64c561e4b8ee225490a
myeongsoo <lyine0924@gmail.com>
2021-11-26 12:12:41 +0900

    종목 선택 버튼을 추가했어요.

---
+++ Tooda/Sources/Scenes/AddStock/View/RateButton.swift
@@ -0,0 +1,62 @@
+//
+//  RateButton.swift
+//  Tooda
+//
+//  Created by Lyine on 2021/11/26.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import UIKit
+
+final class RateButton: UIButton {
+  
+  private enum FontStyle {
+    static let disabledStyle = TextStyle.body(color: .gray2)
+  }
+  
+  private enum BorderStyle {
+    static let disabledColor = UIColor.gray2
+  }
+  
+  let stockState: StockChangeState
+  
+  override var isSelected: Bool {
+    willSet(newValue) {
+      self.buttonLayerDidChanged(by: newValue)
+    }
+  }
+  
+  init(stockState: StockChangeState) {
+    defer {
+      self.configure()
+    }
+    self.stockState = stockState
+    super.init(frame: .zero)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func configure(){
+    self.backgroundColor = .white
+    self.layer.cornerRadius = 10.0
+    self.layer.borderWidth = 1.0
+    self.layer.borderColor = BorderStyle.disabledColor.cgColor
+    
+    self.configureStyle()
+  }
+  
+  private func configureStyle() {
+    
+    let title = self.stockState.titleText
+    
+    self.setAttributedTitle(title.styled(with: FontStyle.disabledStyle), for: .normal)
+    
+    self.setAttributedTitle(title.styled(with: TextStyle.bodyBold(color: self.stockState.titleColor)), for: .selected)
+  }
+  
+  private func buttonLayerDidChanged(by isSelected: Bool) {
+    self.layer.borderColor = isSelected ? self.stockState.titleColor.cgColor : BorderStyle.disabledColor.cgColor
+  }
+}
