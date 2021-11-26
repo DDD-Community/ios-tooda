b21229eb01c4990855467cbf7b2733782f5af8ce
myeongsoo <lyine0924@gmail.com>
2021-11-26 12:01:22 +0900

    종목 변동률 열거형에 Extension 변수를 정의했어요.

--- Tooda/Sources/Entities/Note/NoteStock.swift
+++ Tooda/Sources/Entities/Note/NoteStock.swift
@@ -7,11 +7,36 @@
 //
 
 import Foundation
+import UIKit
 
 enum StockChangeState: String, Codable {
 	case RISE
 	case EVEN
 	case FALL
+}
+
+extension StockChangeState {
+  var titleText: String {
+    switch self {
+      case .RISE:
+        return "+ 상승"
+      case .EVEN:
+        return "유지"
+      case .FALL:
+        return "- 하락"
+    }
+  }
+    
+  var titleColor: UIColor {
+    switch self {
+      case .RISE:
+        return UIColor.subRed
+      case .EVEN:
+        return UIColor.gray2
+      case .FALL:
+        return UIColor.subBlue
+    }
+  }
 }
 
 struct NoteStock: Codable {
