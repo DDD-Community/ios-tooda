a3bd5fe214e0092de9153aefc932da719b363c83
Jaewook <hjw0911@gmail.com>
2021-12-05 21:44:56 +0900

    feat: OneLineReviewPopUpReactor bolierplate 구현

---
+++ Tooda/Sources/Scenes/OneLineReviewPopUp/OneLineReviewPopUpReactor.swift
@@ -0,0 +1,46 @@
+//
+//  OneLineReviewPopUpReactor.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/12/05.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import Foundation
+
+import ReactorKit
+import Then
+import RxRelay
+
+final class OneLineReviewPopUpReactor: Reactor {
+  
+  // MARK: Reactor
+  
+  enum Action {
+    
+  }
+
+  enum Mutation {
+    
+  }
+  
+  struct Dependency {
+    
+  }
+  
+  struct State {
+    
+  }
+
+  // MARK: Properties
+  
+  let dependency: Dependency
+  
+  let initialState: State
+  
+  init(dependency: Dependency) {
+    self.dependency = dependency
+    self.initialState = State()
+  }
+  
+}
