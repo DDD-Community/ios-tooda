e769277bd7ae60e2ba31e7b37b0d399590b22030
Jaewook <hjw0911@gmail.com>
2021-12-05 21:46:10 +0900

    feat: OptionsPopUpView 레이아웃 구현

---
+++ Tooda/Sources/Common/OptionsPopUpView.swift
@@ -0,0 +1,20 @@
+//
+//  OptionsPopUpView.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/12/05.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import UIKit
+
+class OptionsPopUpView: BasePopUpView {
+  
+  // MARK: - UI Components
+  
+  private let tableView = SelfSizingTableView()
+
+  override func setupUI() {
+    super.setupUI()
+  }
+}
