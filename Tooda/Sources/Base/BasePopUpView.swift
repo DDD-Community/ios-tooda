ca10bce47447a0276caf60a631b5ae0d4b592378
Jaewook <hjw0911@gmail.com>
2021-12-05 21:45:17 +0900

    feat: BasePopUpView 구현

---
+++ Tooda/Sources/Base/BasePopUpView.swift
@@ -0,0 +1,55 @@
+//
+//  BasePopUpView.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/12/05.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import UIKit
+
+class BasePopUpView: UIView {
+  
+  // MARK: - Constants
+  
+  private enum Font {
+    static let title = TextStyle.subTitle(color: .gray1)
+  }
+  
+  // MARK: - UI Components
+  
+  private let titleLabel = UILabel().then {
+    $0.textAlignment = .center
+  }
+  
+  private let dismissButton = UIButton(type: .system)
+  
+  private let bottomButton = BaseButton(width: nil, height: 48)
+  
+  func setupUI() {
+    self.do {
+      $0.backgroundColor = .white
+      $0.layer.cornerRadius = 12
+      $0.addSubviews(
+        titleLabel,
+        dismissButton,
+        bottomButton
+      )
+    }
+    
+    titleLabel.snp.makeConstraints {
+      $0.top.equalToSuperview().inset(20)
+      $0.leading.trailing.equalToSuperview().inset(40)
+    }
+    
+    dismissButton.snp.makeConstraints {
+      $0.trailing.equalToSuperview().inset(26)
+      $0.top.equalToSuperview().inset(24)
+      $0.width.height.equalTo(12)
+    }
+    
+    dismissButton.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(21)
+    }
+  }
+}
