e77697bb1dab9f3affee895ebc326cc292d691fe
Jaewook <hjw0911@gmail.com>
2021-12-05 19:09:58 +0900

    feat: SelfSizingTableView 추가

---
+++ Tooda/Sources/Common/SelfSizingTableView.swift
@@ -0,0 +1,15 @@
+//
+//  SelfSizingTableView.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/12/05.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import UIKit
+
+class SelfSizingTableView: UITableView {
+  override var intrinsicContentSize: CGSize {
+    return contentSize
+  }
+}
