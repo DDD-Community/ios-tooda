96495ab28c9f3b97e1974fe59c05bb13aa0b23b5
Jaewook <hjw0911@gmail.com>
2021-12-07 00:45:14 +0900

    feat: TextInputPopUpView 추가 및 레이아웃 구현

---
+++ Tooda/Sources/Common/TextInputPopUpView.swift
@@ -0,0 +1,28 @@
+//
+//  TextInputPopUpView.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2021/12/07.
+//  Copyright © 2021 DTS. All rights reserved.
+//
+
+import UIKit
+
+final class TextInputPopUpView: BasePopUpView {
+  
+  // MARK: - UI Components
+  
+  private let textField = UITextField()
+
+  override func setupUI() {
+    super.setupUI()
+    insertContentViewLayout(
+      view: textField,
+      margin: UIEdgeInsets(top: 20, left: 20, bottom: 26, right: 20)
+    )
+    
+    textField.snp.makeConstraints {
+      $0.height.equalTo(48)
+    }
+  }
+}
