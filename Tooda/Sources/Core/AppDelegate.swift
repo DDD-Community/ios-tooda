03acd599316ec95031243591dcb31b23416bd56c
Jaewook <hjw0911@gmail.com>
2021-12-11 01:20:13 +0900

    feat: Firebase 기본 설정

--- Tooda/Sources/Core/AppDelegate.swift
+++ Tooda/Sources/Core/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import Firebase
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+    FirebaseApp.configure()
 		self.configureTools()
 		return true
 	}
