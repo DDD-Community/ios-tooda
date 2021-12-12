937876f88691fc1feed2aa94b44bdc3030c830de
myeongsoo <lyine0924@gmail.com>
2021-12-12 14:36:51 +0900

    gradient를 만드는 extension 메소드를 정의했어요.

--- Tooda/Sources/Common/Extensions/UIView+Extension.swift
+++ Tooda/Sources/Common/Extensions/UIView+Extension.swift
@@ -13,3 +13,16 @@ extension UIView {
     subviews.forEach(addSubview)
   }
 }
+
+extension UIView {
+  func generateGradient(colors: [UIColor], locations: [NSNumber] = [0, 1], startPoint: CGPoint = CGPoint(x: 0.5, y: 0.0), endPoint: CGPoint = CGPoint(x: 0.5, y: 1.0)) -> CAGradientLayer {
+    let gradient: CAGradientLayer = CAGradientLayer()
+    gradient.frame = self.bounds
+    gradient.colors = colors.map { $0.cgColor }
+    gradient.startPoint = startPoint
+    gradient.endPoint = endPoint
+    gradient.locations = locations
+    
+    return gradient
+  }
+}
