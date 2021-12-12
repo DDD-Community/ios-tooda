fabdc5ac5b3b50210568ba41ab3fb0c0d355fee0
Lian <lyine0924@gmail.com>
2021-12-12 18:14:07 +0900

    [UI] UIView에 Gradient와 Shadow를 적용할 수 있어요. (#99)
    * gradient를 만드는 extension 메소드를 정의했어요.
    
    * UIView에 shadow를 적용하는 Extension 메소드를 추가했어요.

--- Tooda/Sources/Common/Extensions/UIView+Extension.swift
+++ Tooda/Sources/Common/Extensions/UIView+Extension.swift
@@ -13,3 +13,26 @@ extension UIView {
     subviews.forEach(addSubview)
   }
 }
+
+extension UIView {
+  func generateGradient(colors: [UIColor], locations: [NSNumber] = [0, 1], startPoint: CGPoint = CGPoint(x: 0.5, y: 0.0), endPoint: CGPoint = CGPoint(x: 0.5, y: 1.0)) -> CAGradientLayer {
+    let gradient: CAGradientLayer = CAGradientLayer()
+    gradient.frame = self.bounds
+    gradient.colors = colors.map { $0.cgColor }
+    gradient.startPoint = startPoint
+    gradient.endPoint = endPoint
+    gradient.locations = locations
+    
+    return gradient
+  }
+}
+
+extension UIView {
+  func configureShadow(color: UIColor, radius: CGFloat, opacity: Float, offset: CGSize) {
+    self.layer.masksToBounds = false
+    self.layer.shadowColor = color.cgColor
+    self.layer.shadowOpacity = opacity
+    self.layer.shadowOffset = offset
+    self.layer.shadowRadius = radius
+  }
+}
