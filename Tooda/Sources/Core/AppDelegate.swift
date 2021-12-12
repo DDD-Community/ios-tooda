24129bba3c40c7caf5d0c40032319802c66093ab
Jaewook <hjw0911@gmail.com>
2021-12-12 18:17:36 +0900

    chore: 라인 정리

--- Tooda/Sources/Core/AppDelegate.swift
+++ Tooda/Sources/Core/AppDelegate.swift
@@ -14,7 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+	func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
 		// Override point for customization after application launch.
     FirebaseApp.configure()
 		self.configureTools()
