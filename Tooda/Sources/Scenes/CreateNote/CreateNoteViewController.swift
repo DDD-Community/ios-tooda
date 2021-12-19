7f283a34e5c14cd227f914e32f0410bacc4ee9ab
myeongsoo <lyine0924@gmail.com>
2021-12-19 18:31:10 +0900

    노트 등록 화면 하단에 링크 아이콘 관련 이미지를 추가했어요.

--- Tooda/Sources/Scenes/CreateNote/CreateNoteViewController.swift
+++ Tooda/Sources/Scenes/CreateNote/CreateNoteViewController.swift
@@ -18,6 +18,10 @@ class CreateNoteViewController: BaseViewController<CreateNoteViewReactor> {
   typealias Reactor = CreateNoteViewReactor
 
   typealias Section = RxTableViewSectionedReloadDataSource<NoteSection>
+  
+  private enum Metric {
+    static let linkButtonSize: CGFloat = 20.0
+  }
 
   // MARK: Custom Action
   
@@ -87,6 +91,14 @@ class CreateNoteViewController: BaseViewController<CreateNoteViewReactor> {
     $0.register(NoteImageCell.self)
     $0.register(NoteStockCell.self)
   }
+  
+  private let linkContainerView = UIView().then {
+    $0.backgroundColor = .white
+  }
+  
+  private let linkButton = UIButton().then {
+    $0.setImage(UIImage(type: .link), for: .normal)
+  }
 
   // MARK: Initialize
 
@@ -119,6 +131,10 @@ class CreateNoteViewController: BaseViewController<CreateNoteViewReactor> {
     [tableView].forEach {
       self.view.addSubview($0)
     }
+    
+    self.tableView.addSubview(self.linkContainerView)
+    
+    self.linkContainerView.addSubview(self.linkButton)
   }
 
   override func configureConstraints() {
@@ -129,6 +145,18 @@ class CreateNoteViewController: BaseViewController<CreateNoteViewReactor> {
       $0.left.equalToSuperview().offset(14)
       $0.right.equalToSuperview().offset(-14)
       $0.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+    }
+    
+    linkContainerView.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview()
+      $0.bottom.equalTo(self.view.keyboardLayoutGuide.snp.top)
+    }
+    
+    linkButton.snp.makeConstraints {
+      $0.top.equalToSuperview().offset(8)
+      $0.centerY.equalToSuperview()
+      $0.leading.equalToSuperview()
+      $0.size.equalTo(Metric.linkButtonSize)
     }
   }
 
