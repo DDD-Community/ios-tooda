db4ceaff7cfaa49328349c58d6449a5de62add64
Jaewook <hjw0911@gmail.com>
2021-12-30 02:58:31 +0900

    OptionsPopUpView 관련 API 추가

--- Tooda/Sources/Common/OptionsPopUpView.swift
+++ Tooda/Sources/Common/OptionsPopUpView.swift
@@ -8,7 +8,30 @@
 
 import UIKit
 
+import RxDataSources
+import RxSwift
+import RxCocoa
+
 final class OptionsPopUpView: BasePopUpView {
+  
+  // MARK: - Constants
+  
+  typealias Section = RxTableViewSectionedReloadDataSource<EmojiOptionsSectionModel>
+  
+  // MARK: - Rx Stream
+  
+  lazy var didSelectOptionStream = tableView.rx.itemSelected
+  
+  // MARK: - Properties
+  
+  private let dataSource = Section { _, tableView, indexPath, item in
+    let cell = tableView.dequeue(
+      OneLineReviewOptionCell.self,
+      indexPath: indexPath
+    )
+    cell.configure(sticker: item)
+    return cell
+  }
   
   // MARK: - UI Components
   
@@ -22,5 +45,17 @@ final class OptionsPopUpView: BasePopUpView {
       view: tableView,
       margin: UIEdgeInsets(top: 18, left: 0, bottom: 26, right: 0)
     )
+  }
+  
+  // MARK: - Internal methods
+  
+  func registerCell(cellType: UITableViewCell.Type) {
+    tableView.register(cellType)
+  }
+  
+  func bindDataSource(sectionModel: Observable<[EmojiOptionsSectionModel]>) {
+    sectionModel
+      .bind(to: tableView.rx.items(dataSource: dataSource))
+      .disposed(by: disposeBag)
   }
 }
