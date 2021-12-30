2d5bea42c57ff6ebfc535ce10268a0989ef350df
myeongsoo <lyine0924@gmail.com>
2021-12-30 21:07:50 +0900

    LinkPreviewResponse 구조체를 설계하고 LinkService에 적용했어요.

--- Tooda/Sources/Utils/LinkPreviewService.swift
+++ Tooda/Sources/Utils/LinkPreviewService.swift
@@ -7,8 +7,22 @@
 
 import SwiftLinkPreview
 
+struct LinkPreviewResponse {
+  var title: String?
+  var imageURL: String?
+  var canonicalUrl: String?
+  var description: String?
+  
+  init(response: Response) {
+    self.title = response.title
+    self.imageURL = response.image
+    self.canonicalUrl = response.canonicalUrl
+    self.description = response.description
+  }
+}
+
 protocol LinkPreViewServiceType {
-  func fetchMetaData(urlString: String, completion: @escaping (Response?) -> Void)
+  func fetchMetaData(urlString: String, completion: @escaping (LinkPreviewResponse?) -> Void)
 }
 
 final class LinkPreviewService: LinkPreViewServiceType {
@@ -18,12 +32,12 @@ final class LinkPreviewService: LinkPreViewServiceType {
     responseQueue: DispatchQueue.main,
     cache: InMemoryCache())
   
-  func fetchMetaData(urlString: String, completion: @escaping (Response?) -> Void) {
+  func fetchMetaData(urlString: String, completion: @escaping (LinkPreviewResponse?) -> Void) {
     if let cache = self.preview.cache.slp_getCachedResponse(url: urlString) {
-      completion(cache)
+      completion(.init(response: cache))
     } else {
       self.preview.preview(urlString, onSuccess: {
-        completion($0)
+        completion(.init(response: $0))
       }, onError: { _ in
         completion(nil)
       })
