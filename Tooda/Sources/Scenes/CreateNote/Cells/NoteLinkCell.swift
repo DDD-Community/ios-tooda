86e59c2733a3d45629503f10dd0e88802bfbd16c
myeongsoo <lyine0924@gmail.com>
2021-12-31 23:28:03 +0900

    LinkCell의 UI 관련 코드를 구현했어요, 이름도 목적에 맞게 변경했어요.

--- Tooda/Sources/Scenes/CreateNote/Cells/NoteLinkCell.swift
+++ Tooda/Sources/Scenes/CreateNote/Cells/NoteLinkCell.swift
@@ -27,51 +27,63 @@ class NoteLinkCell: BaseTableViewCell, View {
   }
   
   private enum Metric {
-    static let imageViewHeight: CGFloat = 94.0
+    static let imageViewSize: CGSize = CGSize(width: 74.0, height: 74.0)
     static let linkIconSize: CGSize = CGSize(width: 14.0, height: 14.0)
+    static let lineViewWidth: CGFloat = 1.0
   }
   
   // MARK: UI Properties
   
+  private let indcatorView = UIActivityIndicatorView()
+  
   private let containerStackView = UIStackView().then {
     $0.axis = .horizontal
-    $0.alignment = .fill
     $0.layer.cornerRadius = 8.0
-    $0.clipsToBounds = true
     $0.layer.borderColor = UIColor.gray4.cgColor
     $0.layer.borderWidth = 1.0
+    $0.clipsToBounds = true
+    $0.translatesAutoresizingMaskIntoConstraints = false
   }
   
-  private let imageContentView = UIImageView().then {
+  private let thumnailView = UIImageView().then {
     $0.contentMode = .scaleAspectFill
+    $0.clipsToBounds = true
   }
   
-  private let linkInfoStackView = UIStackView().then {
+  private let lineView = UIView().then {
+    $0.backgroundColor = UIColor.gray4
+  }
+  
+  private let infoStackView = UIStackView().then {
     $0.axis = .vertical
-    $0.distribution = .fill
+    $0.distribution = .equalCentering
+    $0.alignment = .leading
     $0.layoutMargins = UIEdgeInsets(top: 8.5, left: 16, bottom: 7.5, right: 15)
     $0.isLayoutMarginsRelativeArrangement = true
+    $0.translatesAutoresizingMaskIntoConstraints = false
   }
   
-  private let linkContainerView = UIStackView().then {
+  private let titleStackView = UIStackView().then {
     $0.axis = .horizontal
     $0.alignment = .center
+    $0.distribution = .fill
     $0.spacing = 3.94
   }
 
-  private let linkIconView = UIImageView().then {
+  private let linkIconView = UIImageView(image: UIImage(type: .link)).then {
     $0.contentMode = .scaleAspectFit
   }
   
-  private let linkTitleLabel = UILabel().then {
+  private let titleLabel = MarginLabel(edgeInsets: UIEdgeInsets(top: 0, left: 0, bottom: 5, right: 0)).then {
+    $0.numberOfLines = Const.labelLineNumbers
+    $0.verticalAlignment = .top
+  }
+  
+  private let descriptionLabel = UILabel().then {
     $0.numberOfLines = Const.labelLineNumbers
   }
   
-  private let descriptionCaptionLabel = UILabel().then {
-    $0.numberOfLines = Const.labelLineNumbers
-  }
-  
-  private let linkCaptionLabel = UILabel().then {
+  private let canonicalLabel = UILabel().then {
     $0.numberOfLines = Const.labelLineNumbers
   }
   
@@ -90,34 +102,41 @@ class NoteLinkCell: BaseTableViewCell, View {
   override func configureUI() {
     super.configureUI()
     
-    self.addSubview(containerStackView)
+    self.contentView.addSubviews(containerStackView, indcatorView)
     
-    [imageContentView, linkInfoStackView].forEach {
+    [thumnailView, lineView, infoStackView].forEach {
       self.containerStackView.addArrangedSubview($0)
     }
     
-    [linkContainerView, descriptionCaptionLabel, linkCaptionLabel].forEach {
-      self.linkInfoStackView.addArrangedSubview($0)
+    [titleStackView, descriptionLabel, canonicalLabel].forEach {
+      self.infoStackView.addArrangedSubview($0)
     }
-    
-    [linkIconView, linkTitleLabel].forEach {
-      self.linkContainerView.addArrangedSubview($0)
+
+    [linkIconView, titleLabel].forEach {
+      self.titleStackView.addArrangedSubview($0)
     }
   }
   
   override func setupConstraints() {
     super.setupConstraints()
     
+    indcatorView.snp.makeConstraints {
+      $0.center.equalToSuperview()
+    }
+    
     containerStackView.snp.makeConstraints {
       $0.top.equalToSuperview().offset(10)
-      $0.leading.trailing.equalToSuperview()
-      $0.height.equalTo(Metric.imageViewHeight)
+      $0.leading.trailing.bottom.equalToSuperview()
     }
     
-    imageContentView.snp.makeConstraints {
-      $0.width.equalTo(Metric.imageViewHeight)
+    thumnailView.snp.makeConstraints {
+      $0.size.equalTo(Metric.imageViewSize)
     }
     
+    lineView.snp.makeConstraints {
+      $0.width.equalTo(Metric.lineViewWidth)
+    }
+        
     linkIconView.snp.makeConstraints {
       $0.size.equalTo(Metric.linkIconSize)
     }
