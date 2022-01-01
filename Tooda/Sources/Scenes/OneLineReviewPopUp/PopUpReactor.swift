d5a314b1a36bcb1ba34729c298077225aa6cc381
Jaewook <hjw0911@gmail.com>
2022-01-01 22:00:29 +0900

    PopUpReactor에 관련 action 및 reduce, mutate 함수를 추가했어요

--- Tooda/Sources/Scenes/OneLineReviewPopUp/PopUpReactor.swift
+++ Tooda/Sources/Scenes/OneLineReviewPopUp/PopUpReactor.swift
@@ -26,10 +26,13 @@ final class PopUpReactor: Reactor {
   
   enum Action {
     case didSelectOption(Int)
+    case didTapBottonButton
+    case didAddTextInput(String)
+    case dismiss
   }
 
   enum Mutation {
-
+    case setSelectedOption(Sticker)
   }
   
   struct Dependency {
@@ -39,6 +42,7 @@ final class PopUpReactor: Reactor {
   
   struct State {
     let emojiOptionsSectionModels: [EmojiOptionsSectionModel]
+    var selectedSticker: Sticker?
     
     static func generateInitialState() -> State {
       return State(
@@ -46,14 +50,15 @@ final class PopUpReactor: Reactor {
           EmojiOptionsSectionModel(
             items: [
               .wow,
+              .thinking,
+              .sad,
               .angry,
               .chicken,
-              .pencil,
-              .thinking,
-              .sad
+              .pencil
             ]
           )
-        ]
+        ],
+        selectedSticker: nil
       )
     }
   }
@@ -76,7 +81,13 @@ extension PopUpReactor {
   func mutate(action: Action) -> Observable<Mutation> {
     switch action {
     case let .didSelectOption(index):
-      return didSelectOption(index: index)
+      return didSelectOptionMutation(index: index)
+    case .didTapBottonButton:
+      return didTapBottonButtonMutation()
+    case let .didAddTextInput(textInput):
+      return didAddTextInputMutation(textInput: textInput)
+    case .dismiss:
+      return dismissMutation()
     }
   }
 }
@@ -85,12 +96,27 @@ extension PopUpReactor {
 
 extension PopUpReactor {
 
-  private func didSelectOption(index: Int) -> Observable<Mutation> {
-    if let selectedEmoji = initialState.emojiOptionsSectionModels.first?.items[safe: index] {
-      
-      if case let .list(optionPublishRelay) = dependency.type {
-        optionPublishRelay.accept(selectedEmoji)
-      }
+  private func didSelectOptionMutation(index: Int) -> Observable<Mutation> {
+    guard let selectedEmoji = initialState.emojiOptionsSectionModels.first?.items[safe: index] else { return Observable<Mutation>.empty() }
+    
+    return Observable<Mutation>.just(.setSelectedOption(selectedEmoji))
+  }
+  
+  private func dismissMutation() -> Observable<Mutation> {
+    
+    dependency.coordinator.close(
+      style: .dismiss,
+      animated: false,
+      completion: nil
+    )
+    
+    return Observable<Mutation>.empty()
+  }
+  
+  private func didTapBottonButtonMutation() -> Observable<Mutation> {
+    if let selectedSticker = currentState.selectedSticker,
+      case let .list(relay) = dependency.type {
+        relay.accept(selectedSticker)
     }
     
     dependency.coordinator.close(
@@ -100,5 +126,35 @@ extension PopUpReactor {
     )
     
     return Observable<Mutation>.empty()
+  }
+  
+  private func didAddTextInputMutation(textInput: String) -> Observable<Mutation> {
+    if case let .textInput(relay) = dependency.type {
+        relay.accept(textInput)
+    }
+    
+    dependency.coordinator.close(
+      style: .dismiss,
+      animated: false,
+      completion: nil
+    )
+    
+    return Observable<Mutation>.empty()
+  }
+}
+
+// MARK: - Reduce
+
+extension PopUpReactor {
+
+  func reduce(state: State, mutation: Mutation) -> State {
+    var newState = state
+
+    switch mutation {
+    case let .setSelectedOption(sticker):
+      newState.selectedSticker = sticker
+    }
+
+    return newState
   }
 }
