002537c149341e63eb4decb0431074d9715f2ad6
myeongsoo <lyine0924@gmail.com>
2022-01-02 21:18:06 +0900

    textView와 field에서 maxLength extension을 제거했어요.

--- Tooda/Sources/Common/UITextField+Extension.swift
+++ Tooda/Sources/Common/UITextField+Extension.swift
@@ -17,38 +17,3 @@ extension UITextField {
     self.attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: [NSAttributedString.Key.foregroundColor: color])
   }
 }
-
-private var kAssociationKeyMaxLength: Int = 0
-
-extension UITextField {
-  
-  var maxLength: Int {
-    get {
-      if let length = objc_getAssociatedObject(self, &kAssociationKeyMaxLength) as? Int {
-        return length
-      } else {
-        return Int.max
-      }
-    }
-    set {
-      objc_setAssociatedObject(self, &kAssociationKeyMaxLength, newValue, .OBJC_ASSOCIATION_RETAIN)
-      addTarget(self, action: #selector(checkMaxLength), for: .editingChanged)
-    }
-  }
-  
-  @objc func checkMaxLength(textField: UITextField) {
-    guard let prospectiveText = self.text,
-          prospectiveText.count > maxLength
-    else {
-      return
-    }
-    
-    let selection = selectedTextRange
-    
-    let indexEndOfText = prospectiveText.index(prospectiveText.startIndex, offsetBy: maxLength)
-    let substring = prospectiveText[..<indexEndOfText]
-    text = String(substring)
-    
-    selectedTextRange = selection
-  }
-}
