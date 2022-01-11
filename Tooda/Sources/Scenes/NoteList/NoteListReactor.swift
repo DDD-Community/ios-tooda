9a21fda092974e805ebfaa06e1e7655b0e71ec21
Jaewook <hjw0911@gmail.com>
2022-01-11 23:42:26 +0900

    NoteListReactor에서 payload 반영하여 API 호출하도록 수정했어요

--- Tooda/Sources/Scenes/NoteList/NoteListReactor.swift
+++ Tooda/Sources/Scenes/NoteList/NoteListReactor.swift
@@ -14,6 +14,11 @@ import RxSwift
 final class NoteListReactor: Reactor {
   
   // MARK: Constants
+
+  struct Payload {
+    let year: Int
+    let month: Int
+  }
   
   enum Constants {
     static let sectionIdentifier = "NoteListSection"
@@ -23,21 +28,27 @@ final class NoteListReactor: Reactor {
   
   enum Action {
     case initialLoad
+    case dismiss
   }
   
   enum Mutation {
     case setIsEmpty(Bool)
+    case setPayload(Payload)
     case setNoteListModel([NoteListModel])
   }
   
   struct Dependency {
     let service: NetworkingProtocol
     let coordinator: AppCoordinatorType
+    let payload: Payload
   }
   
   struct State {
     var noteListModel: [NoteListModel]
     var isEmpty: Bool
+    let fetchWindowSize: Int = 15
+    var cursor: Int?
+    var payload: Payload?
   }
   
   init(dependency: Dependency) {
@@ -48,7 +59,12 @@ final class NoteListReactor: Reactor {
   
   let dependency: Dependency
   
-  let initialState: State = State(noteListModel: [], isEmpty: false)
+  let initialState: State = State(
+    noteListModel: [],
+    isEmpty: false,
+    cursor: nil,
+    payload: nil
+  )
 }
 
 // MARK: - Mutate
@@ -59,25 +75,52 @@ extension NoteListReactor {
     switch action {
     case .initialLoad:
       return loadMutation()
+    case .dismiss:
+      return dismissMutation()
     }
   }
   
+  private func dismissMutation() -> Observable<Mutation> {
+    dependency.coordinator.close(
+      style: .dismiss,
+      animated: true,
+      completion: nil
+    )
+    return Observable<Mutation>.empty()
+  }
+  
   private func loadMutation() -> Observable<Mutation> {
-    return dependency.service.request(NoteAPI.list(limit: 15, cursor: 0))
+    return dependency.service.request(
+      NoteAPI.monthlyList(
+        limit: initialState.fetchWindowSize,
+        cursor: initialState.cursor,
+        year: dependency.payload.year,
+        month: dependency.payload.month
+        )
+      )
       .map([Note].self)
       .asObservable()
-      .map {
-        if $0.isEmpty {
-          return Mutation.setIsEmpty(true)
+      .flatMap { [weak self] noteList -> Observable<Mutation> in
+        guard let self = self else { return Observable<Mutation>.empty() }
+        let payloadStream = Observable.just(Mutation.setPayload(self.dependency.payload))
+        if noteList.isEmpty {
+          return Observable<Mutation>.concat([
+            payloadStream,
+            Observable.just(Mutation.setIsEmpty(true))
+          ])
         } else {
-          return Mutation.setNoteListModel(
+          let noteListModelMutation = Mutation.setNoteListModel(
             [
               NoteListModel(
                 identity: Constants.sectionIdentifier,
-                items: $0
+                items: noteList
               )
             ]
           )
+          return Observable<Mutation>.concat([
+            payloadStream,
+            Observable.just(noteListModelMutation)
+          ])
         }
     }
   }
@@ -96,6 +139,8 @@ extension NoteListReactor {
       newState.noteListModel = model
     case let .setIsEmpty(isEmpty):
       newState.isEmpty = isEmpty
+    case let .setPayload(payload):
+      newState.payload = payload
     }
     
     return newState
