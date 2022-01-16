58ae1978cc21c00cac04773cae194fa5894ae4c1
Jaewook <hjw0911@gmail.com>
2022-01-16 19:57:56 +0900

    라인 정리

--- Tooda/Sources/Entities/Note/Note.swift
+++ Tooda/Sources/Entities/Note/Note.swift
@@ -11,32 +11,32 @@ import Foundation
 import RxDataSources
 
 struct Note: Codable, IdentifiableType, Equatable {
-    static func == (lhs: Note, rhs: Note) -> Bool {
-        return lhs.id == rhs.id
-    }
-
-    var identity: Int {
-        return id
-    }
-	var id: Int
-	var title: String
-	var content: String
-	var createdAt: String?
-	var updatedAt: String?
-	var sticker: Sticker?
-	var noteStocks: [NoteStock]
-	var noteLinks: [NoteLink]?
-	var noteImages: [NoteImage]
-	
-	private enum CodingKeys: String, CodingKey {
-		case id = "id"
-		case title = "title"
-		case content = "content"
-		case sticker = "sticker"
-		case noteStocks = "stocks"
-		case noteLinks = "links"
-		case noteImages = "images"
-		case createdAt = "createdAt"
-		case updatedAt = "updatedAt"
+  static func == (lhs: Note, rhs: Note) -> Bool {
+    return lhs.id == rhs.id
+  }
+  
+  var identity: Int {
+    return id
+  }
+  var id: Int
+  var title: String
+  var content: String
+  var createdAt: String?
+  var updatedAt: String?
+  var sticker: Sticker?
+  var noteStocks: [NoteStock]
+  var noteLinks: [NoteLink]?
+  var noteImages: [NoteImage]
+  
+  private enum CodingKeys: String, CodingKey {
+    case id = "id"
+    case title = "title"
+    case content = "content"
+    case sticker = "sticker"
+    case noteStocks = "stocks"
+    case noteLinks = "links"
+    case noteImages = "images"
+    case createdAt = "createdAt"
+    case updatedAt = "updatedAt"
   }
 }
