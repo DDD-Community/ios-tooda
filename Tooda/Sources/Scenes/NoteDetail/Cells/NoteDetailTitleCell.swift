c429a8b6db65f073de60e65fa178e8821aadf592
Jaewook <hjw0911@gmail.com>
2022-01-22 21:52:52 +0900

    NoteDetailTitleCell 기본 UI를 추가했어요

---
+++ Tooda/Sources/Scenes/NoteDetail/Cells/NoteDetailTitleCell.swift
@@ -0,0 +1,40 @@
+//
+//  NoteDetailTitleCell.swift
+//  Tooda
+//
+//  Created by 황재욱 on 2022/01/22.
+//
+
+import UIKit
+
+final class NoteDetailTitleCell: UITableViewCell {
+
+  private let titleLabel = UILabel()
+  
+  private let dateLabel = UILabel()
+  
+  override init(
+    style: UITableViewCell.CellStyle,
+    reuseIdentifier: String?
+  ) {
+    super.init(
+      style: style,
+      reuseIdentifier: reuseIdentifier
+    )
+  }
+  
+  func configure() {
+    
+  }
+  
+  
+  override func configureUI() {
+    super.configureUI()
+    
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    
+  }
+}
