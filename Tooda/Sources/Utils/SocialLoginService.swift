ff23b8abcf2ef8e51989c0aec3f2ca3646395d3c
Jaewook <hjw0911@gmail.com>
2022-01-23 21:19:59 +0900

    SocialLoginService에 애플 로그인을 구현했어요

--- Tooda/Sources/Utils/SocialLoginService.swift
+++ Tooda/Sources/Utils/SocialLoginService.swift
@@ -7,14 +7,21 @@
 
 import Foundation
 
+import AuthenticationServices
 import RxSwift
 import RxCocoa
+import Moya
+
+protocol SocialLoginServiceType {
+  var tokenProvider: PublishSubject<SocialLoginService.SocialLoginResult> { get }
+  func signIn(type: SocialLoginType)
+}
 
 enum SocialLoginType {
   case apple
 }
 
-final class SocialLoginService {
+final class SocialLoginService: NSObject, SocialLoginServiceType {
   
   enum SocialLoginError: Error {
     case parsingError
@@ -22,9 +29,67 @@ final class SocialLoginService {
   
   typealias SocialLoginResult = (token: String?, type: SocialLoginType, error: Error?)
   
-  private let tokenProvider = PublishRelay<SocialLoginResult>
+  let tokenProvider = PublishSubject<SocialLoginResult>()
+  let disposeBag = DisposeBag()
   
-  init(tokenProvider: PublishRelay<SocialLoginResult>) {
-    self.tokenProvider = tokenProvider
+  let networking = Networking(
+    plugins: [
+      NetworkLoggerPlugin()
+    ]
+  )
+}
+
+extension SocialLoginService {
+  
+  func signIn(type: SocialLoginType) {
+    switch type {
+    case .apple:
+      signInWithApple()
+    }
+  }
+  
+  private func signInWithApple() {
+    let request = ASAuthorizationAppleIDProvider().createRequest()
+    request.requestedScopes = [.fullName, .email]
+
+    let controller = ASAuthorizationController(authorizationRequests: [request])
+    controller.delegate = self
+    controller.presentationContextProvider = self
+    controller.performRequests()
+  }
+}
+
+// MARK: - ASAuthorizationControllerDelegate
+extension SocialLoginService: ASAuthorizationControllerDelegate {
+  func authorizationController(
+    controller: ASAuthorizationController,
+    didCompleteWithAuthorization authorization: ASAuthorization
+  ) {
+    switch authorization.credential {
+      case let appleIdCredential as ASAuthorizationAppleIDCredential:
+        if let token = appleIdCredential.identityToken,
+           let tokenString = String(data: token, encoding: .utf8) {
+
+          tokenProvider.onNext((tokenString, .apple, nil))
+        } else {
+          tokenProvider.onNext((nil, .apple, SocialLoginError.parsingError))
+        }
+      default:
+        break
+    }
+  }
+  
+  func authorizationController(
+    controller: ASAuthorizationController,
+    didCompleteWithError error: Error
+  ) {
+    tokenProvider.onNext((nil, .apple, error))
+  }
+}
+
+// MARK: - ASAuthorizationControllerPresentationContextProviding
+extension SocialLoginService: ASAuthorizationControllerPresentationContextProviding {
+  func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+    return UIApplication.shared.windows.last!
   }
 }
