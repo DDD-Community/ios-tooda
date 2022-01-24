f8e5123efb5bfdf76da63b5b1a61e7dccc869bd0
Jaewook <hjw0911@gmail.com>
2022-01-25 01:35:43 +0900

    NoteDetailReactor에 초기 데이터 로드를 위한 action, mutation을 추가했어요

--- Tooda/Sources/Scenes/NoteDetail/NoteDetailReactor.swift
+++ Tooda/Sources/Scenes/NoteDetail/NoteDetailReactor.swift
@@ -15,22 +15,23 @@ final class NoteDetailReactor: Reactor {
   // MARK: Dependency
 
   struct Dependency {
+    let service: NetworkingProtocol
     let coordinator: AppCoordinatorType
   }
-
 
   // MARK: Reactor
 
   enum Action {
+    case loadData
     case back
   }
 
   enum Mutation {
-
+    case setNoteDetailSectionModel([NoteDetailSection])
   }
 
   struct State {
-
+    var sectionModel: [NoteDetailSection]
   }
 
 
@@ -38,7 +39,7 @@ final class NoteDetailReactor: Reactor {
 
   private let dependency: Dependency
 
-  let initialState: State = State()
+  let initialState: State = State(sectionModel: [])
 
   init(dependency: Dependency) {
     self.dependency = dependency
@@ -52,6 +53,8 @@ extension NoteDetailReactor {
 
   func mutate(action: Action) -> Observable<Mutation> {
     switch action {
+    case .loadData:
+      loadDataMutation()
     case .back:
       self.dependency.coordinator.close(
         style: .pop,
@@ -60,6 +63,27 @@ extension NoteDetailReactor {
       )
       return .empty()
     }
+  }
+  
+  private func loadDataMutation() -> Observable<Mutation> {
+    return dependency.service.request(NoteAPI.detail(id: ""))
+      .map(Note.self)
+      .asObservable()
+      .flatMap { note -> Observable<Mutation> in
+        let sectionModels = [
+          NoteDetailSection(
+            identity: .header,
+            items: [
+              .sticker(note.sticker ?? Sticker.wow),
+              .title(note.title, note.updatedAt),
+              .content(note.content)
+            ]
+          )
+        ]
+        return Observable<Mutation>.just(
+          Mutation.setNoteDetailSectionModel(sectionModels)
+        )
+      }
   }
 }
 
@@ -71,6 +95,11 @@ extension NoteDetailReactor {
   func reduce(state: State, mutation: Mutation) -> State {
     var newState = state
 
+    switch mutation {
+    case let .setNoteDetailSectionModel(sectionModel):
+      newState.sectionModel = sectionModel
+    }
+    
     return newState
   }
 }
