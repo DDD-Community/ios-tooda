f845bd83c2f4e4aa0cffba854262a39caf04f996
jinsu3758 <jinsu3758@naver.com>
2022-02-06 12:26:20 +0900

    updatedAt이 없으면 "오늘 살펴봤어요"를 안 나오게 합니다

--- Tooda/Sources/Scenes/Home/HomeReactor.swift
+++ Tooda/Sources/Scenes/Home/HomeReactor.swift
@@ -363,14 +363,17 @@ extension HomeReactor {
     viewModels.append(
       contentsOf:
         metas.enumerated().map { (index, item) in
-          let day = Calendar.current.dateComponents(
-            [.day],
-            from: item.updatedAt ?? Date(),
-            to: Date()
-          ).day
-
           let historyDate: String? = {
-            guard let day = day, day >= 0 else { return nil }
+            guard let updateAt = item.updatedAt,
+                  let day = Calendar.current.dateComponents(
+                    [.day],
+                    from: updateAt,
+                    to: Date()
+                  ).day,
+                  day >= 0
+            else {
+              return nil
+            }
             guard day > 0 else { return "오늘 살펴봤어요." }
             return "\(day)일 전에 살펴봤어요"
           }()
