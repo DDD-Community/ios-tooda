175df8133da08dbe09092e3e6890d71020f727a6
myeongsoo <lyine0924@gmail.com>
2022-03-26 20:41:35 +0900

    ToodaStep을 추가했어요.

---
+++ Tooda/Sources/Core/Coordinator/Steps/ToodaStep.swift
@@ -0,0 +1,19 @@
+//
+//  ToodaStep.swift
+//  Tooda
+//
+//  Created by Lyine on 2022/03/26.
+//
+
+import Foundation
+import RxFlow
+
+enum ToodaStep: Step {
+	// Global
+	case alert(message: String)
+	case dismiss
+	
+	// Login
+	case loginIsRequired
+	case loginIsCompleted
+}
