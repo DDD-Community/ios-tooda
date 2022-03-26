491d28acb35a75ff5828d4aeb9bfb741179c0d53
myeongsoo <lyine0924@gmail.com>
2022-03-26 22:12:22 +0900

    LoginReactor에 Stepper 프로토콜을 채택하고 코디네이팅 로직을 구현했어요.

--- Tooda/Sources/Scenes/Login/LoginReactor.swift
+++ Tooda/Sources/Scenes/Login/LoginReactor.swift
@@ -11,15 +11,17 @@ import Foundation
 import ReactorKit
 import RxSwift
 import RxCocoa
+import RxFlow
 import Firebase
 import Then
 
-final class LoginReactor: Reactor {
+final class LoginReactor: Reactor, Stepper {
   
   // MARK: - Constants
   
   struct Dependency {
     let service: NetworkingProtocol
+    @available(*, deprecated, message: "RxFlow로 대체될 예정이에요.")
     let coordinator: AppCoordinatorType
     let localPersistanceManager: LocalPersistanceManagerType
     let socialLoginService: SocialLoginServiceType
@@ -47,6 +49,9 @@ final class LoginReactor: Reactor {
   private let disposeBag = DisposeBag()
   
   let initialState: State = State(isAuthorized: false)
+  
+  // MARK: - Stepper
+  var steps: PublishRelay<Step> = .init()
   
   // MARK: - Con(De)structor
   
@@ -121,6 +126,10 @@ extension LoginReactor {
       from: .home,
       shouldNavigationWrapped: true
     )
+    
+    
+    // TODO: 화면 연결이 끝나면 코디네이터 코드를 제거하고 아래 코드로 대체해요.
+//    steps.accept(ToodaStep.loginIsCompleted)
     
     return Observable<Mutation>.empty()
   }
